6a51bd49361b5dc80422dcd9a8bc898e995123f3
jvittone <jules.vittone@laposte.net>
2020-02-11 13:56:33 +0000

    Merde

--- src/fonctionConvertisseur.py
+++ src/fonctionConvertisseur.py
@@ -9,5 +9,39 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import pandas
 
+def FonctionConvertion(temp3,convertion):
+	for i in temp3:
+		for j in convertion:
+			if i[1]==j[0]:
+				i[1]=j[1]
+	return temp3
+
+def FonctionDecoupagePhrase(entree):
+	temp = []
+	for ligne in entree:
+    if len(ligne.split())==2:
+		  temp += [ligne.split()]
+
+	return temp
+
+def ChoixAffiche(temp3):
+	resultat = ""
+	for i in temp3:
+		resultat += i[0] + "	" + i[1] + "\n"
+	return resultat
+
 def convertisseur(source,sortie):
-  source 
+	temp3 = FonctionDecoupagePhrase(entree)
+	
+	#print(pandas.DataFrame({'temp3':temp3}))
+
+	temp3 = FonctionConvertion(temp3,convertion)
+
+	resultat = ChoixAffiche(temp3)
+
+	# print(pandas.DataFrame({'Resultat':resultat}))
+	#print(resultat)
+  
+  numpy.savetxt("../data/pos_reference.txt.univ",[resultat],fmt='%s')
+  
+  
--- src/projet.py
+++ src/projet.py
@@ -9,8 +9,9 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import pandas
 import fonctionConvertisseur as fctConv
+import nltk
 
-source = open("../data/pos_reference.txt.lima","r")
-sortie = open("../data/pos_reference.txt.univ","w")
+source = open("../data/POSTags_PTB_Universal_Linux.txt","r")
+entree = open("../data/pos_reference.txt.lima","w")
 
-fctConv.convertisseur(source,sortie)
+fctConv.convertisseur(source,entree)
