0e76063cd2e9adb4d2ac64171b874a874f1f04ae
jvittone <jules.vittone@laposte.net>
2020-02-11 13:41:59 +0000

    Python

--- src/fonctionConvertisseur.py
+++ src/fonctionConvertisseur.py
@@ -8,3 +8,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import pandas
+
+def convertisseur(source,sortie):
+  source 
--- src/projet.py
+++ src/projet.py
@@ -8,3 +8,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import pandas
+import fonctionConvertisseur as fctConv
+
+source = open("../data/pos_reference.txt.lima","r")
+sortie = open("../data/pos_reference.txt.univ","w")
+
+fctConv.convertisseur(source,sortie)
