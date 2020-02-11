39babccde07a204b3797ffd6221b2702446186c2
jvittone <jules.vittone@laposte.net>
2020-02-11 13:32:00 +0000

    All

---
+++ src/fonctionConvertisseur.py
@@ -0,0 +1,10 @@
+#Import des librairies
+
+import numpy
+import sklearn
+import glob
+import os
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import pandas
